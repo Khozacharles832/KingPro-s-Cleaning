8c47c6559c83e4bd9f1f17f7c648da3e35f909c8
Charlie <khozacharles832@gmail.com>
2026-06-15 16:58:10 +0200

    configure tanstack and zustand

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,13 @@
-import { Stack } from "expo-router";
+import { queryClient } from "@/lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Slot } from "expo-router";
+
 import "../../global.css";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Slot />
+    </QueryClientProvider>
+  );
 }
